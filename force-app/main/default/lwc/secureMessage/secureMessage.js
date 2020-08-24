3f48949fc592f8944fbc442ee4f05b7aa3c46dea
Robert Goodman <robert.goodman@robertgood-ltm.internal.salesforce.com>
2020-08-24 16:04:15 -0400

    Final touches: send message and showToast

--- force-app/main/default/lwc/secureMessage/secureMessage.js
+++ force-app/main/default/lwc/secureMessage/secureMessage.js
@@ -1,16 +1,26 @@
 import { wire, api, track, LightningElement } from 'lwc';
 import qContact  from "@salesforce/apex/secureMessage.qContact";
 import qCase from "@salesforce/apex/secureMessage.qCase";
+import { createRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import SECURE_MESSAGE_OBJECT from '@salesforce/schema/Secure_Message__c';
+import NAME_FIELD from '@salesforce/schema/Secure_Message__c.Name';
+import Case_Id_FIELD from '@salesforce/schema/Secure_Message__c.Case_Id__c';
+import Contact_FIELD from '@salesforce/schema/Secure_Message__c.Contact__c';
+import Body_FIELD from '@salesforce/schema/Secure_Message__c.Body__c';
+
 
 
 export default class SecureMessage extends LightningElement {
     @track screen1 = true;
     @track screen2 = false;
     @track screen3 = false;
-    @track screen4 = false;
 
     @track contactValue = null;
     @track caseValue = null;
+    @track subjectValue = null;
+    @track messageValue = null;
+    @track messageStatus = null;
 
     @track contactData;
     @track contactError;
@@ -59,10 +69,8 @@ export default class SecureMessage extends LightningElement {
     }
 
     HandleNext2(event) {
-        if(this.caseValue != null) {
             this.screen2 = false;
             this.screen3 = true;
-        }
     }
 
     HandleBack2(event) {
@@ -71,7 +79,56 @@ export default class SecureMessage extends LightningElement {
         this.contactValue = null;
     }
 
+    handleSubject(event) {
+        this.subjectValue = event.detail.value;
+    }
+
+    handleMessage(event) {
+        this.messageValue = event.detail.value;
+    } 
+
+
+    sendMessage() {
+        const fields = {};
+        fields[NAME_FIELD.fieldApiName] = this.subjectValue;
+        fields[Case_Id_FIELD.fieldApiName] = this.caseValue;
+        fields[Contact_FIELD.fieldApiName] = this.contactValue;
+        fields[Body_FIELD.fieldApiName] = this.messageValue;
+        const recordInput = {apiName: SECURE_MESSAGE_OBJECT.objectApiName, fields };
+        createRecord(recordInput)
+            .then(result => {
+                this.messageStatus = true;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Message Sent',
+                        variant: 'success',
+                    }),
+                );
+            })
+            .catch(error => {
+                this.messageStatus = false;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error Sending Message',
+                        message: error.body.message,
+                        variant: 'error',
+                    }),
+                );
+            });
+
+    }
+
     HandleNext3(event) {
+        this.sendMessage();
+        if (this.messageStatus) {
+            this.contactValue = null;
+            this.caseValue = null;
+            this.messageValue = null;
+            this.subjectValue = null;
+        }
+        this.screen3 = false;
+        this.screen1 = true;
     }
 
     HandleBack3(event) {
